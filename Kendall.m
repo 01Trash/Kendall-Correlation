84a98264f5645e1b8ae961ef3b2d33d64afc9e21
Erdem <57351007+ErdemGns@users.noreply.github.com>
2022-04-06 19:15:01 +0300

    Update Kendall.m

--- Kendall.m
+++ Kendall.m
@@ -1,19 +1,23 @@
 clc; clear;
 % DN(gun)     X(deniz)       Y(elipsoit)
 
-%%% Verileri *.txt içerisinden okuma yaptır %%%
+%%% Verileri *.txt içerisinden al %%%
 T = readtable('bitirme.txt');
 
 %%% Özellikle işlem yapılmak istenilen yerleri al %%%
+% DN = gün
+DN = T(1:5,1:1);
 % X = deniz yüksekliği
 X = T(1:5,2:2);
 % Y = elipsoit yüksekliği
 Y = T(1:5,3:3);
 
 %%% table to array yap %%%
-% X değerlerini array yap
+% DN table to array yap
+DN = table2array(DN);
+% X table to array yap
 X = table2array(X);
-% Y değerlerini array yap
+% Y table to array yap
 Y = table2array(Y);
 
 
